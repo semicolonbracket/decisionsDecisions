619d3e27d07383a7405babd711d3a2dd6583316d
lna24 <105398388+lna24@users.noreply.github.com>
2022-09-19 16:45:44 +1000

    Initial Commit

---
+++ decisionsDecisions/decisionsDecisionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  decisionsDecisionsApp.swift
+//  decisionsDecisions
+//
+//  Created by Geena Deierl on 19/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct decisionsDecisionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
