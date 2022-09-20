c560d94e479f394ead2dfa9fd515dffda03a9908
semicolonbracket <geena@deierl.com>
2022-09-20 22:31:38 +1000

    Changed deployment target to 15.6 so it could be simulated on my personal device. Added key functionalities and basic design for the "Quick Decision" option.

--- decisionsDecisions/QuickDecisionView.swift
+++ decisionsDecisions/QuickDecisionView.swift
@@ -8,8 +8,50 @@
 import SwiftUI
 
 struct QuickDecisionView: View {
+    @State private var input = ""
+    @State private var inputs:[String] = []
+    @State private var result = ""
+    
     var body: some View {
-        Text("Quick View!")
+        VStack{
+            Text("Quick Decision!")
+                .font(.title2)
+                .fontWeight(.heavy)
+            HStack(){
+                TextField("Insert option here", text: $input)
+                    .multilineTextAlignment(.center)
+                    .border(/*@START_MENU_TOKEN@*/Color.gray/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+                    .padding(.leading)
+                Button("ENTER") {
+                    inputs.append(input)
+                    input = ""
+                    for i in inputs{
+                        print(i)
+                    }
+                }
+                .padding(.trailing, 30.0)
+                //.padding(.leading, -20.0)
+                .buttonStyle(.bordered)
+                .tint(.blue)
+            }
+            Button("RANDOMISE") {
+                result = inputs.randomElement() ?? "invalid input:("
+            }
+            .buttonStyle(.bordered)
+            .tint(.purple)
+            //.bold() - only for min IOS 16.0 deployment
+            .font(.headline)
+            Button("RESET") {
+                inputs = []
+                input = ""
+                result = ""
+            }
+            .buttonStyle(.bordered)
+            .tint(.red)
+            Text(result)
+                .fontWeight(.semibold)
+                .padding(.top, 30.0)
+        }
     }
 }
 
