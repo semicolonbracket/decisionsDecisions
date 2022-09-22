4db0eda4130f08dc590f880c0a20fc5ba6aee962
semicolonbracket <geena@deierl.com>
2022-09-22 12:11:34 +1000

    Achieved continuous rotation for a sample colour wheel (for "Wheel Spinner" Option)

--- decisionsDecisions/WheelSpinnerView.swift
+++ decisionsDecisions/WheelSpinnerView.swift
@@ -8,8 +8,24 @@
 import SwiftUI
 
 struct WheelSpinnerView: View {
+    @State private var spin = false
     var body: some View {
-        Text("Wheel Spinner")
+        VStack{
+            Text("Wheel Spinner")
+                .font(.title2)
+                .fontWeight(.heavy)
+            Button("Spin me") {
+                spin.toggle()
+            }
+            .buttonStyle(.bordered)
+            .tint(.purple)
+            .font(.headline)
+            Image("480px-Eight-colour-wheel-2D")
+                .rotationEffect(.degrees(spin ? 360 : 0))
+                //image rotates in the opposite direction every 2nd button press, potentially a result of the rotation in degrees^ being a stored variable
+                .animation(.linear.repeatCount(5, autoreverses: false).speed(0.5), value: spin)
+                .foregroundColor(.gray)
+        }
     }
 }
 
